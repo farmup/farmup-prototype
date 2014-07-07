4a3baa288e0ccba621c2ac5c9c9c830a430b9439
Luis Mendoza <luis@mendoza.io>
2014-07-07 18:51:25 -0400

    Página inicio, plugins y estructura.

---
+++ js/main.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function() {
+ 
+  $("#sliderProductos").owlCarousel({
+    slideSpeed : 300,
+    paginationSpeed : 400,
+    itemsCustom : [
+        [0, 1],
+        [600, 2],
+        [1000, 3],
+        [1200, 4]
+      ]
+  });
+
+  $('.owl-pagination').addClass('hidden');
+
+  var owl = $(".owl-carousel").data('owlCarousel');
+
+  $('#sliderPrev').click(function(){
+  	owl.prev();
+  });
+
+  $('#sliderNext').click(function(){
+  	owl.next();
+  });
+ 
+});
+
